98ddd1e87cc4134f3a2c4317b50c6eb655aaf88d
Stefan Kendall <stefankendall@gmail.com>
2013-10-27 20:41:13 -0400

    Making colors more readable.

--- PokemonTypeEffectiveness/Views/Type/TypeEffectivenessCell.m
+++ PokemonTypeEffectiveness/Views/Type/TypeEffectivenessCell.m
@@ -12,7 +12,7 @@
 - (UIColor *)colorForType:(NSString *)type {
     return @{
             @"normal" : [UIColor darkTextColor],
-            @"fighting" : [UIColor colorWithRed:192 / 255.0f green:48 / 255.0f blue:40 / 255.0f alpha:1.0f],
+            @"fighting" : [UIColor colorWithRed:175/255.0f green:0/255.0f blue:0/255.0f alpha:1.0f],
             @"flying" : [UIColor colorWithRed:168 / 255.0f green:144 / 255.0f blue:240 / 255.0f alpha:1.0f],
             @"poison" : [UIColor colorWithRed:160 / 255.0f green:64 / 255.0f blue:160 / 255.0f alpha:1.0f],
             @"ground" : [UIColor colorWithRed:146/255.0f green:125/255.0f blue:68/255.0f alpha:1.0f],
@@ -20,7 +20,7 @@
             @"bug" : [UIColor colorWithRed:109/255.0f green:120/255.0f blue:21/255.0f alpha:1.0f],
             @"ghost" : [UIColor colorWithRed:112 / 255.0f green:88 / 255.0f blue:152 / 255.0f alpha:1.0f],
             @"steel" : [UIColor colorWithRed:156/255.0f green:156/255.0f blue:156/255.0f alpha:1.0f],
-            @"fire" : [UIColor colorWithRed:240 / 255.0f green:128 / 255.0f blue:48 / 255.0f alpha:1.0f],
+            @"fire" : [UIColor colorWithRed:234/255.0f green:0/255.0f blue:0/255.0f alpha:1.0f],
             @"water" : [UIColor blueColor],
             @"grass" : [UIColor colorWithRed:0/255.0f green:132/255.0f blue:0/255.0f alpha:1.0f],
             @"electric" : [UIColor colorWithRed:226/255.0f green:219/255.0f blue:0/255.0f alpha:1.0f],
